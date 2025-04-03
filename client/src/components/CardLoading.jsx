3827e51313ab3e8611a571df94a262f3177e93a3
Dark Shadow <dark256.256.256.256shadow@gmail.com>
2025-04-04 02:32:07 +0530

    Update CardLoading.jsx

--- client/src/components/CardLoading.jsx
+++ client/src/components/CardLoading.jsx
@@ -2,7 +2,7 @@
 
 const CardLoading = () => {
     return (
-        <div className="border p-4 grid gap-1 lg:max-w-52 min-w-36 rounded-md animate-pulse">
+       <div className="border p-4 grid gap-1 lg:max-w-52 min-w-36 rounded-md animate-pulse">
             <div className="min-h-16 lg:min-h-24  bg-blue-50">
 
             </div>
@@ -17,9 +17,9 @@ const CardLoading = () => {
             </div>
 
             <div className="flex items-center justify-between gap-3">
-                <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
+                <div className="p-2 lg:p-3 bg-blue-50 rounded w-10 lg:w-16"></div>
 
-                <div className="p-2 lg:p-3 bg-blue-50 rounded w-20"></div>
+                <div className="p-2 lg:p-3 bg-blue-50 rounded w-10 lg:w-16"></div>
             </div>
 
 
