9ed3e0e152d90f27f68d3ecaf153529af78d76f8
Dark Shadow <dark256.256.256.256shadow@gmail.com>
2025-04-04 03:51:21 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <footer className='lg:bg-gray-900 lg:text-white text-slate-800 p-[17.5px] '>
       <div className='container mx-auto text-center flex flex-col lg:flex-row lg:justify-between gap-2 text-base'>
-        <p>&copy; 2024 E-Bazar. All rights reserved.</p>
+        <p>&copy; 2025 E-Bazar. All rights reserved.</p>
 
         <div className="flex items-center justify-center gap-4 text-2xl">
           <a href="#" className="hover:text-blue-700">
@@ -19,7 +19,7 @@ const Footer = () => {
           <a href="#" className="hover:text-pink-500">
             <FaInstagram />
           </a>
-          <a href="#" className="hover:text-blue-400">
+          <a href="www.linkedin.com/in/suvrodip-chakroborty-0aa21b264" className="hover:text-blue-400">
             <FaLinkedin />
           </a>
           <a href="#" className="hover:text-gray-700">
@@ -28,7 +28,7 @@ const Footer = () => {
           <a href="#" className="hover:text-violet-500">
             <FaGithubAlt />
           </a>
-          <a href="#" className="hover:text-emerald-500">
+          <a href="https://suvrodip-chakroborty.vercel.app/" className="hover:text-emerald-500">
             <TbWorldWww />
           </a>
         </div>
